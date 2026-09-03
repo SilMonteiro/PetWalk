25e96283c95b317821ec11417bff5bb535709455
Silvia Monteiro <silvia.monteiro800@gmail.com>
2026-09-03 20:25:11 -0300

    adicionando classes avalicao e passeio

---
+++ backend/Projeto.Api/Dominio/AvaliacaoPasseador.cs
@@ -0,0 +1,5 @@
+class AvaliacaoPasseador 
+{
+    public int Nota {get; set;} = 0;
+    public string Comentario {get; set;} = "";
+}
---
+++ backend/Projeto.Api/Dominio/Passeio.cs
@@ -0,0 +1,7 @@
+class Passeio 
+{
+    public string Data {get; set;} = "";
+    public string Local {get; set;} = "";
+    public bool Aceito {get; set;} = false;
+    public bool Finalizado {get; set;} = false;
+}
