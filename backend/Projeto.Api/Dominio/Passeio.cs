1c6ed3b9ddddff6a6333c3610fe708571ce3f3eb
Guilherme Inácio <guiinacio04@gmail.com>
2026-09-26 16:25:22 -0300

    Configura API do backend

--- backend/Projeto.Api/Dominio/Passeio.cs
+++ backend/Projeto.Api/Dominio/Passeio.cs
@@ -1,8 +1,8 @@
 public class Passeio
 {
     public int Id { get; set; }
-    public DateTime DataHora { get; set; } = ;
-    public TimeSpan Duracao { get; set; } = ;
+    public DateTime DataHora { get; set; } 
+    public TimeSpan Duracao { get; set; } 
     public string Local { get; set; } = "";
 
     public int TutorId { get; set; }
@@ -10,4 +10,15 @@ public class Passeio
     public int PetId { get; set; }
 
     public StatusPasseio Status { get; set; } = StatusPasseio.SOLICITADO;
+
+    public Passeio(Tutor tutor, Passeador passeador, Pet pet, DateTime dataHora, TimeSpan duracao, string local)
+    {
+        TutorId = tutor.Id;
+        PasseadorId = passeador.Id;
+        PetId = pet.Id;
+        DataHora = dataHora;
+        Duracao = duracao;
+        Local = local;
+        Status = StatusPasseio.SOLICITADO;
+    }
 }
